ca028feceb98ab63ea6379b848ec5ecbe9888216
madforre <33324825+madforre@users.noreply.github.com>
2018-04-07 22:33:47 +0900

    index css추가
    index 거의 완성단계인듯

--- index.php
+++ index.php
@@ -14,14 +14,14 @@ $arrSec[0]=array( 'title'=>'PLEASURE',
 // sec2
 $arrSec[1]=array( 'title'=>'INNOVATION',
                   'h1_copy'=>"새로운 꿈이 현실이 되는 곳",
-                  'h1_comment' =>'다양한 기업이 꿈을 키워 나가는 곳
+                  'h1_comment' =>'다양한 기업이 꿈을 키워 나가는 곳<br>
                   혁신의 시작을 함께하는 곳, 63입니다.',
                   'btnLink' => '/'
                  );
 // sec3
-$arrSec[2]=array('title'=>'ABOUT',
+$arrSec[2]=array('title'=>'ABOUT 63',
                   'h1_copy'=>"아름다운 한강변에 반사되는 금빛 파사드",
-                  'h1_comment' =>'1985년 완공 이후 서울을 대표하는 상징적인 건축물로
+                  'h1_comment' =>'1985년 완공 이후 서울을 대표하는 상징적인 건축물로<br>
                   햇빛에 따라 반사되는 금빛 외관이 인상적입니다',
                   'btnLink' => '/'
                  );
@@ -29,7 +29,7 @@ $arrSec[2]=array('title'=>'ABOUT',
 $arrSec[3]=array(
                  'title'=>'FESTIVAL',
                  'h1_copy'=>"우리가 함께 할 감동의 순간들",
-                 'h1_comment' =>'봄 내음 가득한 벚꽃축제, 가을하늘을 수놓은 불꽃축제가
+                 'h1_comment' =>'봄 내음 가득한 벚꽃축제, 가을하늘을 수놓은 불꽃축제가<br>
                  빛나는 추억의 순간으로 함께합니다',
                  'btnLink' => '/'
                   );
@@ -47,6 +47,7 @@ $arrSec[3]=array(
 </head>
 
 <body>
+  <div id="wrap">
    <div class="header">
        <div class="nav">
             <div class="menu left"></div>
@@ -56,13 +57,12 @@ $arrSec[3]=array(
    </div>
 
    <div class="section">
-       <ul>
-        <?php
 
+        <?php
           $i=1;
         foreach($arrSec as $key){
             echo "
-            <li class=sec".$i.">
+            <div class='section sec$i'>
               <div class='contents'>
                   <h1 class='title'>
                         ".$key['title']."
@@ -78,25 +78,27 @@ $arrSec[3]=array(
                         <a href=".$key['btnLink']."><span>알아보기</span></a>
                     </div>
                 </div>
-            </li>"
+            </div>"
             ;
             $i++;
           };
-
         ?>
-       </ul>
-       <div class="side_bar">
-           <span class="active"></span>
-           <span></span>
-           <span></span>
-           <span></span>
-       </div>
+
+    </div>
+
+  <div class="side_bar">
+      <span class="active"></span>
+      <span></span>
+      <span></span>
+      <span></span>
   </div>
 
-
-   <div class="footer">
-
-
-   </div>
+<?php
+  echo "
+ <div class='footer'>
+  ㅎㅎ 나는 푸터
+ </div>
+</div>
+ "; ?>
 </body>
 </html>
